94144e39bb04e0dd0e2e752a2d757330ac0e13d0
Jeff Mcmahan <jeff@Jeffs-MacBook-Pro.local>
2018-11-16 10:14:17 -0500

    Added note capability to returns()

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const callStack = require('./call-stack')
 const noop = () => void 0
 
 // API surface
+exports.types 	= require('./types')
 exports.fn 		= require('./fn')
 exports.desc 	= noop
 exports.note 	= noop
 exports.assert 	= f => callStack.slice(-1)[0].fn.onReturn = f
 exports.error 	= f => callStack.slice(-1)[0].fn.onError = f
 exports.param 	= val => Type => (check('param')(Type)(val), noop)
-exports.returns = Type => callStack.slice(-1)[0].fn.type = check('function')(Type)
-exports.types 	= require('./types')
+exports.returns = Type => (
+	callStack.slice(-1)[0].fn.type = check('function')(Type),
+	noop
+)
 
 // Dynamically export the types at the top-level.
 Object.keys(exports.types).forEach(key => exports[key] = exports.types[key])
