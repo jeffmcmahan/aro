eff922d0a79cf0a9bb4f34aeeb1a6209405f4a37
Jeff Mcmahan <jeff@Jeffs-MacBook-Pro.local>
2018-11-30 22:40:50 -0500

    fixed browser-only bug

--- utils/mode.js
+++ utils/mode.js
@@ -1,14 +1,15 @@
 'use strict'
 
+// Determines whether the tool should be on or off, depending on
+// the environment and some flags/variables/etc.
 module.exports = () => {
-	if ((typeof process !== 'undefined') && (process.release.name === 'node')) {
+	const browser = (typeof process === 'undefined') || !process.release
+	if (browser) {
+		return window['--development'] ? 'on' : 'off'
+	} else {
 		const nodeInspect = process.execArgv.some(arg => arg.includes('--inspect-brk='))
 		const nodeEnv = process.env.NODE_ENV === 'development'
 		const devFlag = process.argv.includes('--development')
-		return (nodeInspect || nodeEnv || devFlag)
-			? 'on'
-			: 'off'
-	} else {
-		return window['--development'] ? 'on' : 'off'
+		return (nodeInspect || nodeEnv || devFlag) ? 'on' : 'off'
 	}
 }
